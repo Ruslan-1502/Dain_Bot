0eb0005862eae144f4da01842bce98619a0038c2
Ruslan <rkv15.02@mail.ru>
2023-05-16 04:32:51 +0500

    Add web process

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 
 
-BOT_TOKEN = os.environ.get('5905267839:AAGjbqOOXQlSAPYPQZj7TWx5rCf-DDWeDT0')
+BOT_TOKEN = '5905267839:AAGjbqOOXQlSAPYPQZj7TWx5rCf-DDWeDT0'
 WEBHOOK_URL_PATH = os.environ.get('WEBHOOK_URL_PATH')
 WEBAPP_HOST = os.environ.get('WEBAPP_HOST', '0.0.0.0')
 WEBAPP_PORT = os.environ.get('PORT')
