1cc77470059dabc6eadbea3dd44a1e3177f4e5bf
Ruslan <rkv15.02@mail.ru>
2023-05-16 03:48:38 +0500

    Add web process

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-import logging
-import sqlite3
+import os
+from aiohttp import web
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.types import ParseMode
+from aiogram.dispatcher.webhook import SendMessage
 from aiogram.utils import executor
-from config import BOT_TOKEN
 
-logging.basicConfig(level=logging.INFO)
-
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
-dp.middleware.setup(LoggingMiddleware())
+from config import BOT_TOKEN, WEBHOOK_URL_PATH, WEBAPP_HOST, WEBAPP_PORT
 
 # Создание базы данных
+import sqlite3
 conn = sqlite3.connect("users.db")
 cursor = conn.cursor()
 cursor.execute("""CREATE TABLE IF NOT EXISTS users
                   (id INTEGER PRIMARY KEY, username TEXT, uid INTEGER, ar INTEGER, nick TEXT, region TEXT)
                """)
 
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
 
 def get_region(uid):
     first_digit = int(str(uid)[0])
@@ -32,40 +29,43 @@ def get_region(uid):
         return "sar"
     else:
         return "unknown"
+    
+async def on_startup(app):
+    await bot.delete_webhook()
+    await bot.set_webhook(WEBHOOK_URL_PATH)
 
-def get_main_keyboard():
-    keyboard = types.InlineKeyboardMarkup()
-    add_uid_button = types.InlineKeyboardButton("Добавить UID", callback_data="add_uid")
-    donate_button = types.InlineKeyboardButton("Донат", callback_data="donate")
-    keyboard.add(add_uid_button, donate_button)
-    return keyboard
+async def on_shutdown(app):
+    await bot.delete_webhook()
 
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-     button_add = types.KeyboardButton('Добавить UID')
-     button_donat = types.KeyboardButton('Донат')
-     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).add(button_add, button_donat)
-     start_text = (
+async def handle(request):
+    if request.match_info.get('token') == BOT_TOKEN:
+        update = types.Update.parse_raw(await request.read())
+        await dp.process_update(update)
+        return web.Response(text="OK")
+    else:
+        return web.Response(text="Invalid token")
+
+async def add_uid_callback(query: types.CallbackQuery):
+    await bot.answer_callback_query(query.id)
+    await bot.send_message(chat_id=query.from_user.id, text="Пожалуйста, отправьте свой UID, AR и ник в игре в формате:\nUID AR Nick\nПример: `123456789 45 Player`")
+
+async def donate_callback(query: types.CallbackQuery):
+    await bot.answer_callback_query(query.id)
+    await bot.send_message(chat_id=query.from_user.id, text="https://t.me/genshin_donation/6")
+
+async def start_command(message: types.Message):
+    button_add = types.KeyboardButton('Добавить UID')
+    button_donate = types.KeyboardButton('Донат')
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).add(button_add, button_donate)
+    start_text = (
         "Добро пожаловать! Воспользуйтесь кнопками ниже или командами:\n\n"
         "/uid - Показать список всех игроков\n"
         "/uid @nickname - Показать информацию об игроке с данным ником\n"
         "/uid <region> - Показать список игроков для указанного региона (america, europe, asia, sar)\n"
     )
-    #keyboard = get_main_keyboard()
-     await message.reply(start_text, reply_markup=keyboard)
-    
-@dp.callback_query_handler(lambda c: c.data == "add_uid")
-async def add_uid_callback(callback_query: types.CallbackQuery):
-    await bot.answer_callback_query(callback_query.id)
-    await callback_query.message.reply("Пожалуйста, отправьте свой UID, AR и ник в игре в формате:\nUID AR Nick\nПример: `123456789 45 Player`")
+    await message.answer(start_text, reply_markup=keyboard)
 
-@dp.callback_query_handler(lambda c: c.data == "donate")
-async def donate_callback(callback_query: types.CallbackQuery):
-    await bot.answer_callback_query(callback_query.id)
-    await callback_query.message.reply("https://t.me/genshin_donation/6")
-
-@dp.message_handler(commands=["uid"])
-async def uid(message: types.Message):
+async def uid_command(message: types.Message):
     args = message.get_args().split()
     if len(args) == 0:
         cursor.execute("SELECT * FROM users ORDER BY ar DESC")
@@ -76,34 +76,43 @@ async def uid(message: types.Message):
         region = args[0]
         cursor.execute("SELECT * FROM users WHERE region=? ORDER BY ar DESC", (region,))
     else:
-        await message.reply("Неправильный формат команды. Попробуйте еще раз.")
+        await message.answer("Неправильный формат команды. Попробуйте еще раз.")
         return
 
     result = cursor.fetchall()
     if len(result) == 0:
-        await message.reply("Не найдено пользователей.")
+        await message.answer("Не найдено пользователей.")
         return
 
     output = ""
     for row in result:
         ar, uid, nick = row[3], row[2], row[4]
         output += f"AR: {ar} UID: `{uid}` Nick: {nick}\n"
-        
-    print(output)
 
-    await message.reply(output, parse_mode=ParseMode.MARKDOWN_V2)
+    await message.answer(output, parse_mode=types.ParseMode.MARKDOWN_V2)
 
+@dp.callback_query_handler(lambda c: c.data == "add_uid")
+async def add_uid_callback_handler(callback_query: types.CallbackQuery):
+    await add_uid_callback(callback_query)
 
+@dp.callback_query_handler(lambda c: c.data == "donate")
+async def donate_callback_handler(callback_query: types.CallbackQuery):
+    await donate_callback(callback_query)
 
+@dp.message_handler(commands=['start'])
+async def start_command_handler(message: types.Message):
+    await start_command(message)
 
+@dp.message_handler(commands=["uid"])
+async def uid_command_handler(message: types.Message):
+    await uid_command(message)
 
 @dp.message_handler(lambda message: message.text.startswith("/delete"))
-async def delete(message: types.Message):
+async def delete_handler(message: types.Message):
     uid = int(message.text.split()[1])
     cursor.execute("DELETE FROM users WHERE uid=?", (uid,))
     conn.commit()
-    await message.reply("Пользователь с указанным UID удален из списка.")
-
+    await message.answer("Пользователь с указанным UID удален из списка.")
 
 @dp.message_handler()
 async def process_uid_message(message: types.Message):
@@ -113,7 +122,7 @@ async def process_uid_message(message: types.Message):
         if len(str(uid)) != 9 or not (1 <= ar <= 60):
             raise ValueError("Неправильный формат UID или AR.")
     except ValueError:
-        await message.reply("Пожалуйста, отправьте UID, AR и ник в формате:\nUID AR Nick\nПример: `123456789 45 Player`", parse_mode=ParseMode.MARKDOWN)
+        await message.answer("Пожалуйста, отправьте UID, AR и ник в формате:\nUID AR Nick\nПример: `123456789 45 Player`", parse_mode=types.ParseMode.MARKDOWN)
         return
 
     username = message.from_user.username
@@ -121,8 +130,11 @@ async def process_uid_message(message: types.Message):
 
     cursor.execute("INSERT INTO users (username, uid, ar, nick, region) VALUES (?, ?, ?, ?, ?)", (username, uid, ar, nick, region))
     conn.commit()
-    await message.reply("Ваш UID, AR и ник успешно добавлены в список.")
+    await message.answer("Ваш UID, AR и ник успешно добавлены в список.")
 
-if __name__ == "__main__":
-    from aiogram import executor
-    executor.start_polling(dp)
+app = web.Application()
+app.add_routes([web.post('/{token}', handle)])
+
+if __name__ == '__main__':
+    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
+    web.run_app(app, host=WEBAPP_HOST, port=WEBAPP_PORT)
