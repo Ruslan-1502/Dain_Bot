ad260f612c17f73c6a3e2e80dbc9384050bd3f21
Ruslan <rkv15.02@mail.ru>
2023-05-17 08:18:59 +0500

    Add web process

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 HEROKU_APP_NAME = os.getenv('dain')
 BOT_TOKEN = '5905267839:AAGjbqOOXQlSAPYPQZj7TWx5rCf-DDWeDT0'
 WEBHOOK_URL = f'https://dain.herokuapp.com/webhook/5905267839:AAGjbqOOXQlSAPYPQZj7TWx5rCf-DDWeDT0'
-WEBAPP_HOST = 'https://dain.herokuapp.com'
+WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = os.environ.get('PORT')
 WEBHOOK_PATH = '/webhook/5905267839:AAGjbqOOXQlSAPYPQZj7TWx5rCf-DDWeDT0'
