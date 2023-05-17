1a5634b8eedea8e570f657697784ec8606b9a841
Ruslan <rkv15.02@mail.ru>
2023-05-17 22:12:12 +0500

    Add web process

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ async def handle(request):
 
 async def start_command(message: types.Message):
     button_add = types.KeyboardButton('Добавить UID')
-    button_donate = types.KeyboardButton('Донат',url="https://t.me/genshin_donation/6")
+    button_donate = types.KeyboardButton('Донат')
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).add(button_add, button_donate)
     start_text = (
         "Добро пожаловать! Воспользуйтесь кнопками ниже или командами:\n\n"
@@ -71,33 +71,8 @@ async def add_uid_callback(query: types.CallbackQuery):
     await bot.send_message(chat_id=query.from_user.id, text="Пожалуйста, отправьте свой UID, AR и ник в игре в формате:\nUID AR Nick\nПример: `123456789 45 Player`")
 
 async def donate_callback(query: types.CallbackQuery):
-    await query.message.answer(text="https://t.me/genshin_donation/6")
-
-async def uid_command(message: types.Message):
-    args = message.get_args().split()
-    if len(args) == 0:
-        cursor.execute("SELECT * FROM users ORDER BY ar DESC")
-    elif len(args) == 1 and args[0].startswith("@"):
-        username = args[0][1:]
-        cursor.execute("SELECT * FROM users WHERE username=?", (username,))
-    elif len(args) == 1:
-        region = args[0]
-        cursor.execute("SELECT * FROM users WHERE region=? ORDER BY ar DESC", (region,))
-    else:
-        await message.answer("Неправильный формат команды. Попробуйте еще раз.")
-        return
-
-    result = cursor.fetchall()
-    if len(result) == 0:
-        await message.answer("Не найдено пользователей.")
-        return
-
-    output = ""
-    for row in result:
-        ar, uid, nick = row[3], row[2], row[4]
-        output += f"AR: {ar} UID: `{uid}` Nick: {nick}\n"
-
-    await message.answer(output, parse_mode=types.ParseMode.MARKDOWN_V2)
+    await bot.answer_callback_query(query.id)
+    await bot.send_message(chat_id=query.from_user.id, text="https://t.me/genshin_donation/6")
 
 @dp.callback_query_handler(lambda c: c.data == "add_uid")
 async def add_uid_callback_handler(callback_query: types.CallbackQuery):
@@ -111,9 +86,10 @@ async def donate_callback_handler(callback_query: types.CallbackQuery):
 async def start_command_handler(message: types.Message):
     await start_command(message)
 
-@dp.message_handler(commands=["uid"])
-async def uid_command_handler(message: types.Message):
-    await uid_command(message)
+# Добавляем обработчик кнопки "Донат"
+@dp.message_handler(lambda message: message.text == "Донат")
+async def donate_button_handler(message: types.Message):
+    await donate_callback(types.CallbackQuery.from_message(message))
 
 @dp.message_handler(lambda message: message.text.startswith("/delete"))
 async def delete_handler(message: types.Message):
