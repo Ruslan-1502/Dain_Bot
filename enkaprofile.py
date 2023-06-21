0ba30ec708d8964b96ead87174eefd6281572155
Ruslan-1502 <82501852+Ruslan-1502@users.noreply.github.com>
2023-06-21 05:01:49 +0500

    Add files via upload

---
+++ enkaprofile.py
@@ -0,0 +1,8 @@
+from enkacard import encbanner
+import asyncio
+
+async def encprofile(uid):
+    async with encbanner.ENC() as encard:
+        ENCpy = await encard.enc(uids=uid)
+        profile = await encard.profile(enc=ENCpy, image=True)
+    return profile
