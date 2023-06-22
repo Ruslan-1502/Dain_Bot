633a81aac5dc061015a835515d829e3784c68cae
Ruslan-1502 <82501852+Ruslan-1502@users.noreply.github.com>
2023-06-22 10:29:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -144,29 +144,32 @@ async def uid_command(message: types.Message):
         await message.answer("Не найдено пользователей.")
         return
 
-    output = ""
-    if message.chat.type in ["group", "supergroup"]:
-        keyboard = InlineKeyboardMarkup()
-        for row in result:
-            ar, uid, nickname, username = row[3], row[2], row[4], row[1]
-            output += f"AR: {ar} UID: `{uid}` Nick: {nickname}\n"
-            if show_details:
-                if show_details:
-                    output += f"[Подробнее](https://enka.network/u/{uid})\n"
-                    result = await encprofile(uid)
-                    if 'img' in result:
-                        photo = result['img']
-                        image_output = BytesIO()
-                        photo.save(image_output, format='PNG')
-                        image_output.seek(0)
-                        await bot.send_photo(chat_id=message.chat.id, photo=image_output)
-        keyboard.add(InlineKeyboardButton(f"Добавить свой UID", url=f"https://t.me/akashauz_bot"))
-        await message.answer(output, reply_markup=keyboard, parse_mode=types.ParseMode.MARKDOWN_V2)
+output = ""
+escaped_output = ""  # Initialize the variable with a default value
+
+if message.chat.type in ["group", "supergroup"]:
+    keyboard = InlineKeyboardMarkup()
+    for row in result:
+        ar, uid, nickname, username = row[3], row[2], row[4], row[1]
+        # Here, escape only the parts that are not links.
+        output += markdown.escape(f"AR: {ar} UID: {uid} Nick: {nickname}\n")
+        if show_details:
+            output += f"[Подробнее](https://enka.network/u/{uid})\n"
+            result = await encprofile(uid)
+            if 'img' in result:
+                photo = result['img']
+                image_output = BytesIO()
+                photo.save(image_output, format='PNG')
+                image_output.seek(0)
+                await bot.send_photo(chat_id=message.chat.id, photo=image_output)
+    keyboard.add(InlineKeyboardButton(f"Добавить свой UID", url=f"https://t.me/akashauz_bot"))
+    await message.answer(output, reply_markup=keyboard, parse_mode=types.ParseMode.MARKDOWN_V2)
     else:
         photo = None  # Initialize the variable with a default value
         for row in result:
             ar, uid, nickname, chat_id = row[3], row[2], row[4], row[6]
-            output += f"AR: {ar} UID: `{uid}` Nick: [{nickname}](tg://user?id={chat_id})\n"
+            # Here, escape only the parts that are not links.
+            output += markdown.escape(f"AR: {ar} UID: {uid} Nick: ") + f"[{nickname}](tg://user?id={chat_id})\n"
             if show_details:
                 output += f"[Подробнее](https://enka.network/u/{uid})\n"
                 result = await encprofile(uid)
