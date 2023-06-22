a9cdd77372b14638af3cfac3fdf0e982d79e2463
Ruslan-1502 <82501852+Ruslan-1502@users.noreply.github.com>
2023-06-22 15:23:42 +0500

    Update enkaprofile.py

--- enkaprofile.py
+++ enkaprofile.py
@@ -2,7 +2,11 @@ from enkacard import encbanner
 import asyncio
 
 async def encprofile(uid):
+    profile = None
     async with encbanner.ENC() as encard:
         ENCpy = await encard.enc(uids=uid)
-        profile = await encard.profile(enc=ENCpy, image=True)
+        try:
+            profile = await encard.profile(enc=ENCpy, image=True)
+        except AttributeError:
+            print("Карточка игрока не найдена")  # Вместо print может быть любая другая обработка
     return profile
