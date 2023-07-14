b61252b0b0ea8429da9c31d539743de73cd5a7f0
Ruslan-1502 <82501852+Ruslan-1502@users.noreply.github.com>
2023-07-15 03:05:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -271,12 +271,12 @@ async def saytlar_command(message: types.Message):
         '@akashauz_bot - Добавтить UID',
         't.me/genshin_donation - Донат',
         '@genshin_gaydlar',
-        'Интерактивная карта - <a href="https://act.hoyolab.com/ys/app/interactive-map/index.html?bbs_presentation_style=no_header&lang=ru-ru">ссылка</a>',
+        'Интерактивная карта - [ссылка](https://act.hoyolab.com/ys/app/interactive-map/index.html?bbs_presentation_style=no_header&lang=ru-ru)',
         
         
     ]
     response = '\n'.join(sites)
-    await message.answer(response)
+    await message.answer(response, disable_web_page_preview=True)
 
 # Обработчик команды /bot
 @dp.message_handler(commands=['bot'])
