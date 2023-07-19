d2e5780d6e6c962765f98a96f3e9ff3c200870e5
Ruslan-1502 <82501852+Ruslan-1502@users.noreply.github.com>
2023-07-19 11:50:09 +0500

    Update enkaprofile.py

--- enkaprofile.py
+++ enkaprofile.py
@@ -2,7 +2,7 @@
 from enkacard import encbanner
 import asyncio
 
-async def card(uid):
+async def encprofile(uid):
     async with encbanner.ENC() as encard:
         ENCpy = await encard.enc(uids=uid)
         return await encard.creat(ENCpy, 1)
