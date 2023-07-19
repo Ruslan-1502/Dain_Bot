d0985fe1ba9f6e8bbca625efd6bf41b60bd6662d
Ruslan-1502 <82501852+Ruslan-1502@users.noreply.github.com>
2023-07-20 01:42:26 +0500

    Update enkaprofile.py

--- enkaprofile.py
+++ enkaprofile.py
@@ -5,6 +5,7 @@ import asyncio
 async def encprofile(uid):
     profile = None
     async with encbanner.ENC() as encard:
+        uid = int(uid)
         ENCpy = await encard.enc(uids=uid)
         profile = await encard.profile(enc=ENCpy, image=True)
     return profile
