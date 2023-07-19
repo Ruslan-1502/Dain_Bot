f88b29986aa6d0aab30ceab0787485684153ca3c
Ruslan-1502 <82501852+Ruslan-1502@users.noreply.github.com>
2023-07-20 01:21:59 +0500

    Update enkaprofile.py

--- enkaprofile.py
+++ enkaprofile.py
@@ -7,7 +7,7 @@ async def encprofile(uid):
     async with encbanner.ENC() as encard:
         try:
             ENCpy = await encard.enc(uids=uid)
-            profile = await encard.profile(enc=ENCpy, image=True)
+            profile = await encard.create(ENCpy, 1)
         except AttributeError:
             print(f"Карточка игрока не найдена для uid {uid}")  # Выводим uid с ошибкой
         except Exception as e:
