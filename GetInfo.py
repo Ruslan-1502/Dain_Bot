a11197368d330a039f09218070a029780c3a5b82
Ruslan <rkv15.02@mail.ru>
2023-07-21 22:02:13 +0500

    fix_GetInfo

--- GetInfo.py
+++ GetInfo.py
@@ -8,14 +8,14 @@ client = EnkaNetworkAPI()
 async def get_player(uid):
     try:
         async with client:
-            data = await client.fetch_user(uid)
+            data = await client.fetch_user_by_username(uid)
             return data.player
     except asyncio.TimeoutError:
         print("Timeout error occurred")
         return None
-    except ValueError:
-        print("Value error occurred")
-        return None
+    # except ValueError:
+    #     print("Value error occurred")
+    #     return None
     except EnkaPlayerNotFound:
         print("Player not found")
         return None
