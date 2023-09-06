264084ede34655f710cc5eea6b6dc2e6054daed6
Ruslan <rkv15.02@mail.ru>
2023-09-06 23:40:47 +0500

    change_hook

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 BOT_TOKEN = '5929211650:AAHC5PDld7ESFqwe9IcX4ccGir0eVDHgfPM' #akasha
-WEBHOOK_URL = f'https://akashabot.herokuapp.com/webhook/5929211650:AAHC5PDld7ESFqwe9IcX4ccGir0eVDHgfPM'
+WEBHOOK_URL = f'https://akashabot-b8b0b2aff382.herokuapp.com/webhook/5929211650:AAHC5PDld7ESFqwe9IcX4ccGir0eVDHgfPM'
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = os.environ.get('PORT')
 WEBHOOK_PATH = '/webhook/5929211650:AAHC5PDld7ESFqwe9IcX4ccGir0eVDHgfPM'
