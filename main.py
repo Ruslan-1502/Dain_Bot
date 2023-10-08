a57d3523a271dcaf4ed5ededfe6b94fdafabf3ae
Ruslan <rkv15.02@mail.ru>
2023-10-09 00:08:10 +0500

    change_uid_command

--- main.py
+++ main.py
@@ -141,49 +141,31 @@ async def uid_command(message: types.Message):
         await message.answer("Неправильный формат команды. Попробуйте еще раз.")
         return
 
-    # Получение всех chat_id из вашей базы данных
-    cursor.execute("SELECT chat_id FROM users")
-    users_in_db = cursor.fetchall()
+    cursor.execute("SELECT * FROM users")
+    all_users_data = cursor.fetchall()
 
-    users_in_group = []
-    for user_chat_id_tuple in users_in_db:
-        user_chat_id = user_chat_id_tuple[0]
+    users_data_group = []
+
+    for user_data in all_users_data:
+        chat_id = user_data[0]  # это предположение, в зависимости от структуры вашей БД
         try:
-            member = await bot.get_chat_member(chat_id=current_chat_id, user_id=user_chat_id)
+            member = await bot.get_chat_member(chat_id=current_chat_id, user_id=chat_id)
             if member.status in ['member', 'administrator', 'creator']:
-                users_in_group.append(user_data)
-        except Exception as e:
-            # Пользователь не найден в группе или другая ошибка
+                users_data_group.append(user_data)
+        except Exception:
             pass
-    users_in_group.sort(key=lambda x: (-x[3], x[2])) # предполагая что x[3] это ar и x[2] это uid
-        
-    # Теперь у вас есть список users_in_group с пользователями, которые есть в группе
+
+    # Сортировка списка по ar и uid
+    users_data_group.sort(key=lambda x: (-x[3], x[2]))  # предполагая что x[3] это ar и x[2] это uid
+
     output = ""
     keyboard = InlineKeyboardMarkup()
-    for user_chat_id in users_in_group:
-        if len(args) == 0:
-            cursor.execute("SELECT * FROM users WHERE chat_id=?", (user_chat_id,))
-        else:
-            query = args[0]
-            if query.startswith("@"):
-                username = query[1:]
-                cursor.execute("SELECT * FROM users WHERE username=? AND chat_id=?", (username, user_chat_id))
-                show_details = True
-            elif query in ["asia", "euro", "america", "sar"]:
-                region = query
-                cursor.execute("SELECT * FROM users WHERE region=? AND chat_id=?", (region, user_chat_id))
-            else:
-                first_name = query
-                cursor.execute("SELECT * FROM users WHERE first_name=? AND chat_id=?", (first_name, user_chat_id))
-                show_details = True
-
-        user_data = cursor.fetchone()
-        if user_data:
-            ar, uid, nickname, username = user_data[3], user_data[2], user_data[4], user_data[1]
-            nickname = nickname.replace("#", "")
-            output += f"AR: {ar} UID: <code>{uid}</code> Nick: {nickname}\n"
-            if show_details:
-                output += f"Чтобы посмотреть персонажей <code>/card {uid}</code> "
+    for user_data in users_data_group:
+        ar, uid, nickname, username = user_data[3], user_data[2], user_data[4], user_data[1]
+        nickname = nickname.replace("#", "")
+        output += f"AR: {ar} UID: <code>{uid}</code> Nick: {nickname}\n"
+        if show_details:
+            output += f"Чтобы посмотреть персонажей <code>/card {uid}</code> "
 
     if output:
         keyboard.add(InlineKeyboardButton(f"Добавить свой UID", url=f"https://t.me/akashauz_bot"))
