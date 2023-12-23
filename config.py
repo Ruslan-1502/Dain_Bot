17cdef61d1aa14bc94799aa85528ebb24db636a6
Ruslan <rkv15.02@mail.ru>
2023-12-23 15:14:52 +0500

    change_config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 BOT_TOKEN = '5929211650:AAHC5PDld7ESFqwe9IcX4ccGir0eVDHgfPM' #akasha
 WEBHOOK_URL = f'https://akasha2-d9f20c8dbbd0.herokuapp.com/webhook/5929211650:AAHC5PDld7ESFqwe9IcX4ccGir0eVDHgfPM'
 WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.environ.get('PORT')
+WEBAPP_PORT = int(os.environ.get('PORT'))
 WEBHOOK_PATH = '/webhook/5929211650:AAHC5PDld7ESFqwe9IcX4ccGir0eVDHgfPM'
 
 host = 'kilichev.mysql.pythonanywhere-services.com'
