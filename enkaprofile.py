962ede64db3cf01e7a7c099eb8992e6ba2bd16be
Ruslan <rkv15.02@mail.ru>
2024-01-07 02:48:52 +0500

    enkacard_update

--- enkaprofile.py
+++ enkaprofile.py
@@ -1,17 +1,20 @@
 from enkacard import encbanner
 import asyncio
 
-async def encprofile(uid):
-    # async with encbanner.ENC(uid = uids) as encard:
-    #     return await encard.profile(card = True)
-    profile = None
-    async with encbanner.ENC() as encard:
-        try:
-            await encbanner.update()
-            ENCpy = await encard.enc(uids=uid)
-            profile = await encard.profile(enc=ENCpy, image=True)
-        except AttributeError:
-            print(f"Карточка игрока не найдена для uid {uid}")  # Выводим uid с ошибкой
-        except Exception as e:
-            print(f"Произошла ошибка с uid {uid}: {e}")  # Обработка других ошибок и вывод uid
-    return profile
+async def encprofile(uids):
+    await encbanner.update()
+    async with encbanner.ENC(uid=uids) as encard:
+        image = await encard.profile(card=True, teamplate=1)
+        card_image = image.card
+        return card_image
+    # profile = None
+    # async with encbanner.ENC() as encard:
+    #     try:
+    #         await encbanner.update()
+    #         ENCpy = await encard.enc(uids=uid)
+    #         profile = await encard.profile(enc=ENCpy, image=True)
+    #     except AttributeError:
+    #         print(f"Карточка игрока не найдена для uid {uid}")  # Выводим uid с ошибкой
+    #     except Exception as e:
+    #         print(f"Произошла ошибка с uid {uid}: {e}")  # Обработка других ошибок и вывод uid
+    # return profile
