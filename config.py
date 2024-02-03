2759e1a906acff834c6b16e230ccc89fc3c8aad0
Ruslan <rkv15.02@mail.ru>
2024-02-04 00:31:14 +0500

    changa_url

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 BOT_TOKEN = '5929211650:AAHC5PDld7ESFqwe9IcX4ccGir0eVDHgfPM' #akasha
-WEBHOOK_URL = f'https://akasha2-d9f20c8dbbd0.herokuapp.com/webhook/5929211650:AAHC5PDld7ESFqwe9IcX4ccGir0eVDHgfPM'
+WEBHOOK_URL = f'https://akasha3-ab3148aeef2e.herokuapp.com/webhook/5929211650:AAHC5PDld7ESFqwe9IcX4ccGir0eVDHgfPM'
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = int(os.environ.get('PORT'))
 WEBHOOK_PATH = '/webhook/5929211650:AAHC5PDld7ESFqwe9IcX4ccGir0eVDHgfPM'
