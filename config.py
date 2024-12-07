942b77d3263929a5d090c9adff2d06f066406d4b
Ruslan-1502 <82501852+Ruslan-1502@users.noreply.github.com>
2024-12-07 11:37:58 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 BOT_TOKEN = '5929211650:AAHC5PDld7ESFqwe9IcX4ccGir0eVDHgfPM' #akasha
-WEBHOOK_URL = f'https://akasha3-ab3148aeef2e.herokuapp.com/webhook/5929211650:AAHC5PDld7ESFqwe9IcX4ccGir0eVDHgfPM'
+WEBHOOK_URL = f'https://api.render.com/deploy/srv-ct9ul1ij1k6c738es900?key=miCm8z0SG1I'
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = int(os.environ.get('PORT'))
-WEBHOOK_PATH = '/webhook/5929211650:AAHC5PDld7ESFqwe9IcX4ccGir0eVDHgfPM'
+WEBHOOK_PATH = '/deploy/srv-ct9ul1ij1k6c738es900?key=miCm8z0SG1I'
 
 host = 'kilichev.mysql.pythonanywhere-services.com'
 user = 'kilichev'
